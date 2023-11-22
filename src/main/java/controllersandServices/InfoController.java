bfd4c02d074d85881a73ad92cf9c521f243e1d54
dabi <dabich626626@gmail.com>
2023-11-22 17:18:22 +0200

    ver1

--- src/main/java/controllersandServices/InfoController.java
+++ src/main/java/controllersandServices/InfoController.java
@@ -2,6 +2,9 @@ package controllersandServices;
 
 import org.springframework.beans.factory.annotation.Value;
 
+import java.util.List;
+import java.util.stream.Stream;
+
 @RestController
 @RestMapping("/info")
 public class InfoController {
@@ -16,5 +19,15 @@ public class InfoController {
     @GetMapping("/getPort")
     public int port(){
         return port;
+    }
+
+    @GetMapping("/test")
+    public int test(){
+        long startTime = System.currentTimeMillis();
+
+        int a = integer.valueOf(5);
+        int sum = Stream.iterate(1,a -> a + 1).limit(1_000_000).reduce(0,(a,b) -> a + b);
+        logger.info("Elapsed: {}", System.currentTimeMillis() - startTime);
+        return sum;
     }
 }
